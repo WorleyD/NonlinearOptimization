fed48e819af4ae6db4ba6829063c86db5812b4e8
David Worley <worleyd@uwindsor.ca>
2020-03-29 21:40:01 -0400

    Changed Netwons E

--- ComputationalAssignment/ComputationalAssignment/Newtons/E.py
+++ ComputationalAssignment/ComputationalAssignment/Newtons/E.py
@@ -1,7 +1,7 @@
 import numpy as np
 from math import exp, sqrt
 
-epsilon = 0.000001
+epsilon = 0.0001
 
 
 def norm(x):
@@ -13,19 +13,19 @@ def f(x1,x2):
 
 
 def gradient(x1,x2):
-	x_1 = sum([(-4*i-4*i*i)*exp(i*x1) + i*exp(2*i*x1) - i*exp(i*(x1+x2)) for i in range(1,11)])
-	x_2 = sum([(-4*i-4*i*i)*exp(i*x2) + i*exp(2*i*x2) - i*exp(i*(x1+x2)) for i in range(1,11)])
+	x_1 = sum([2*((2+2*i - exp(i*x1) - exp(i*x2))*(-1*i*exp(i*x1))) for i in range(1,11)])
+	x_2 = sum([2*((2+2*i - exp(i*x1) - exp(i*x2))*(-1*i*exp(i*x2))) for i in range(1,11)])
 	return np.array([x_1, x_2])
 
 def hessian(x1,x2):
-	x11 = sum([(-4*i*i -4*i*i*i)*exp(i*x1) + 2*i*i*exp(2*1*x1) - i*exp(i*(x1+x2)) for i in range(1,11)])
-	x12 = sum([-i*i*exp(i*(x1+x2)) for i in range(1,11)])
+	x11 = sum([2*(2+2*i - exp(i*x1) - exp(i*x2))*(-1*i*i*exp(i*x1)) + 2*(-1*i*exp(i*x1)**2) for i in range(1,11)])
+	x12 = sum([2*(-1*i*exp(i*x1))*(-1*i*exp(i*x2)) for i in range(1,11)])
 	x21 = x12
-	x22 =sum([(-4*i*i -4*i*i*i)*exp(i*x2) + 2*i*i*exp(2*1*x2) - i*exp(i*(x1+x2)) for i in range(1,11)])
+	x22 = sum([2*(2+2*i - exp(i*x1) - exp(i*x2))*(-1*i*i*exp(i*x2)) + 2*(-1*i*exp(i*x2)**2) for i in range(1,11)])
 	return np.array([[x11,x12],[x21,x22]])
 
 
-xk = np.array([-10.0,-10.0])
+xk = np.array([5.0,5.0])
 iterations = 1
 while True:
 	prev = np.array([x for x in xk])
